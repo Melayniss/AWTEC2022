f0d5bdc8f3ed7bf80a6e64de9cb1bb9afd97e5e5
Melayniss <1550372227@qq.com>
2020-09-19 14:00:55 +0800

    New Workplace

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,7 @@
       <div class="container">
         <div class="pull-left">
           <span class="glyphicon glyphicon-user"></span>Contact us by
-          <span class="glyphicon glyphicon-earphone"></span>13758255738
+          <span class="glyphicon glyphicon-earphone"></span>xxxx-xxxxxxxx
           <span class="glyphicon glyphicon-envelope"></span>zhangdahai@zju.edu.cn
         </div>
       </div>
@@ -26,13 +26,13 @@
           @click="navClick(index,item.name)"
         >
           <router-link :to="item.path">
-            {{item.name}}
+            {{ item.name }}
             <span v-if="item.children.length>0" class="glyphicon glyphicon-menu-down"></span>
             <i class="underline"></i>
           </router-link>
           <dl v-if="item.children.length>0">
             <dt v-for="(i,n) in item.children" :key="n">
-              <router-link :to="i.path">{{i.name}}</router-link>
+              <router-link :to="i.path">{{ i.name }}</router-link>
             </dt>
           </dl>
         </li>
@@ -45,7 +45,7 @@
       </div>
       <!-- 导航栏 -->
       <div class="header-nav-m-menu text-center">
-        {{menuName}}
+        {{ menuName }}
         <div
           class="header-nav-m-menu-wrapper"
           data-toggle="collapse"
@@ -65,7 +65,7 @@
             data-target="#menu"
           >
             <router-link :to="item.path">
-              {{item.name}}
+              {{ item.name }}
               <i class="underline"></i>
             </router-link>
           </li>
@@ -151,7 +151,7 @@ export default {
   methods: {
     navClick(index, name) {
       this.navIndex = index;
-      sessionStorage.setItem('navIndex',index)
+      sessionStorage.setItem('navIndex', index)
       this.menuName = name;
     },
     menuClick() {
@@ -170,6 +170,7 @@ export default {
   background: #f4f4f4;
   transition: all ease 0.6s;
 }
+
 #header .header-top {
   height: 50px;
   color: #fff;
@@ -177,14 +178,17 @@ export default {
   line-height: 50px;
   background: #134857;
 }
+
 /* 顶部的图标 */
 #header .header-top span {
   margin: 0 8px;
 }
+
 /* 导航栏 */
 #header .header-nav {
   height: 110px;
 }
+
 /* 导航栏logo */
 #header .header-nav .header-nav-logo {
   width: 100px;
@@ -192,6 +196,7 @@ export default {
   float: left;
   position: relative;
 }
+
 /* 导航栏logo图片 */
 #header .header-nav .header-nav-logo img {
   width: 215px;
@@ -203,24 +208,28 @@ export default {
   bottom: 0;
   margin: auto;
 }
+
 /*暂时如此 logo摆放位置还有待修改*/
 
 /* 导航栏 导航容器 */
 #header .header-nav-fixed .header-nav-wrapper {
   line-height: 50px;
 }
+
 #header .header-nav .header-nav-wrapper {
   line-height: 110px;
   float: right;
   margin: 0;
   max-width: 800px;
 }
+
 /* 导航栏 每个导航 */
 #header .header-nav .header-nav-wrapper > li {
   float: left;
   margin: 0 15px;
   position: relative;
 }
+
 /* 导航栏 每个导航下面的 a 链接 */
 #header .header-nav .header-nav-wrapper > li > a {
   color: #000;
@@ -229,6 +238,7 @@ export default {
   padding: 15px 0;
   position: relative;
 }
+
 /* 导航栏 每个导航下面的 a 链接的下划线 */
 #header .header-nav .header-nav-wrapper > li > a > i {
   display: block;
@@ -241,32 +251,38 @@ export default {
   transition: all 0.6s ease;
   background-color: #93b5cf;
 }
+
 /* 导航栏 每个导航下面的 a 链接的右侧小三角 */
 #header .header-nav .header-nav-wrapper > li > a > span {
   font-size: 12px;
   transition: transform ease 0.5s;
 }
+
 /* 导航栏 每个导航下面的 a 链接 鼠标滑上去的样式 */
 #header .header-nav .header-nav-wrapper > li > a:hover {
   color: #93b5cf;
   text-decoration: none;
 }
+
 /* 导航栏 每个导航下面的 a 链接 鼠标滑上去下划线的样式 */
 #header .header-nav .header-nav-wrapper > li > a:hover .underline {
   opacity: 1;
   width: 100%;
   left: 0;
 }
+
 /* 导航栏 每个导航下面的 a 链接 鼠标滑上去三角标的样式 */
 #header .header-nav .header-nav-wrapper > li > a:hover span {
   transform: rotate(180deg);
 }
+
 /* 导航栏 每个导航下面的 a 链接 鼠标点击后的样式 */
 #header .header-nav .header-nav-wrapper > li.active > a {
   color: #93b5cf;
   text-decoration: none;
   border-bottom: 2px solid #93b5cf;
 }
+
 /* 导航栏 每个导航下面的二级导航容器 */
 #header .header-nav .header-nav-wrapper > li > dl {
   display: none;
@@ -278,33 +294,40 @@ export default {
   box-shadow: 0 0 3px 1px #c4d7d6;
   background: #fff;
 }
+
 /* 导航栏 每个导航下面的二级导航容器的每个导航 */
 #header .header-nav .header-nav-wrapper > li > dl > dt {
   width: 100%;
   padding: 10px;
   border-bottom: 1px solid #c4d7d6;
 }
+
 /* 导航栏 每个导航下面的二级导航容器的每个导航 当鼠标滑上时的样式*/
 #header .header-nav .header-nav-wrapper > li > dl > dt > a:hover {
   text-decoration: none;
 }
+
 /* 导航栏 滑上一级导航显示二级导航 */
 #header .header-nav .header-nav-wrapper > li:hover dl {
   display: block;
 }
+
 #header .header-nav .header-nav-wrapper > li > dl > dt:hover {
   cursor: pointer;
   background: #c4d7d6;
 }
+
 @media screen and (max-width: 997px) {
   #header .header-nav-m {
     position: relative;
   }
+
   /* 导航栏logo容器 */
   #header .header-nav-m .header-nav-m-logo {
     height: 100px;
     position: relative;
   }
+
   /* 导航栏logo图片 */
   #header .header-nav-m .header-nav-m-logo img {
     width: 215px;
@@ -316,6 +339,7 @@ export default {
     bottom: 0;
     margin: auto;
   }
+
   /* 导航栏  菜单容器 */
   #header .header-nav-m .header-nav-m-menu {
     color: #fff;
@@ -325,6 +349,7 @@ export default {
     background: #134857;
     position: relative;
   }
+
   /* 导航栏 菜单图标 */
   #header .header-nav-m .header-nav-m-menu-wrapper {
     position: absolute;
@@ -337,6 +362,7 @@ export default {
     z-index: 999999;
     font-size: 12px;
   }
+
   /* 导航栏 */
   #header .header-nav-m .header-nav-m-wrapper {
     position: absolute;
@@ -346,12 +372,14 @@ export default {
     background: #134857;
     z-index: 9999999;
   }
+
   /* 导航栏 每个导航 */
   #header .header-nav-m .header-nav-m-wrapper > li {
     height: 40px;
     line-height: 40px;
     border-bottom: 1px solid #c4d7d6;
   }
+
   /* 导航栏 每个导航下面的 a 链接 */
   #header .header-nav-m .header-nav-m-wrapper > li > a {
     color: #fff;
@@ -360,6 +388,7 @@ export default {
     padding: 15px 0;
     position: relative;
   }
+
   /* 导航栏 每个导航下面的 a 链接的右侧小三角 */
   #header .header-nav .header-nav-wrapper > li > a > span {
     font-size: 10px;
